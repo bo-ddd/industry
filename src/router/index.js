3dad2693ea9d78ab53054ac3f990c81079feda61
shangyuhao <834725611@qq.com>
2023-02-24 12:53:13 +0800

    add router

--- src/router/index.js
+++ src/router/index.js
@@ -11,10 +11,42 @@ const routes = [
     name: 'Layout',
     component: Layout,
     children: [{
-      path: '/home',
-      name: 'Home',
-      component: ()=>import('../views/home/HomeView.vue') ,
-    }]
+      // 园区经济管理  < 经济管理
+      path: '/EconomyManage',
+      name: 'EconomyManage',
+      component: () => import("../views/ParkEconomyManage/EconomyManage.vue")
+    }, {
+      // 安全生产管理  > 风险分级管控
+      path: '/riskControl',
+      name: 'RiskControl',
+      component: () => import("../views/safetyProductionManagement/riskControl/RiskControl.vue")
+    }, {
+      // 安全生产管理  > 安全防护
+      path: '/safetyProtection',
+      name: 'SafetyProtection',
+      component: () => import("../views/safetyProductionManagement/SafetyProtection/SafetyProtection.vue")
+    }, {
+      // 安全生产管理  > 运输管理
+      path: '/transportationView',
+      name: 'TransportationView',
+      component: () => import("../views/safetyProductionManagement/TransportationView/TransportationView.vue")
+    }, {
+      // 安全生产管理  > 人员管理
+      path: '/userManagement',
+      name: 'UserManagement',
+      component: () => import("../views/safetyProductionManagement/UserManagement/UserManagement.vue")
+    }, {
+      // 安全生产管理  > 安全生产信息
+      path: '/prdInfo',
+      name: 'PrdInfo',
+      component: () => import("../views/safetyProductionManagement/PrdInfo/PrdInfo.vue")
+    }, {
+      // 安全生产管理  > 安全生产监管
+      path: '/SuperviseView',
+      name: 'SuperviseView',
+      component: () => import("../views/safetyProductionManagement/SuperviseView/SuperviseView.vue")
+    }
+    ]
   },
 ]
 
