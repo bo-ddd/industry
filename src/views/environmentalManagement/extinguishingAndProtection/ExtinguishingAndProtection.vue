cc7c5ea592452917ca9398ee92dd46f83b3a4e8a
zhujiangyan <t2710086269@163.com>
2023-02-25 13:05:31 +0800

    污染源...

---
+++ src/views/environmentalManagement/extinguishingAndProtection/ExtinguishingAndProtection.vue
@@ -0,0 +1,34 @@
+<!-- 消防管理 -->
+<template>
+    <div>
+        <img class="img" src="@/assets/images/xf.png" alt="">
+    </div>
+</template>
+  
+<script>
+
+export default {
+    data() {
+        return {
+          
+        }
+    },
+    mounted() {
+    
+    },
+    methods: { }
+}
+
+</script>
+  
+<style scoped>
+div{
+    font-size: 0;
+}
+.img{
+    width: 100%;
+    height: 100vh;
+}
+
+</style>
+  
