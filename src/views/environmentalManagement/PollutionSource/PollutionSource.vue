04ab1bdcec12b66306e09d6000e15fda345c6de9
zhujiangyan <t2710086269@163.com>
2023-02-25 16:46:59 +0800

    在线污染源

--- src/views/environmentalManagement/PollutionSource/PollutionSource.vue
+++ src/views/environmentalManagement/PollutionSource/PollutionSource.vue
@@ -12,31 +12,87 @@
       />
     </div>
     <div class="main">
-      <div class="item">
-        <!-- 站点检测 -->
-        <dv-scroll-board :config="config" style="width: 50rem; height: 100%" />
-      </div>
-      <div class="item"></div>
-      <div class="item"></div>
-      <div class="item">
-        <!-- 24小时趋势图 -->
-        <div id="linechart"></div>
-      </div>
-      <div class="item"></div>
-      <div class="item flex-ja-center">
-        <!-- 重点污染物 -->
-        <div id="keypollutants"></div>
-        <div class="keypollutants-title">
-          天跑1号结炉 425.39/500(报警mg/m"凯气化数 <br>
-
-          国货定电度气 408.8/11500报警m'/s 度气
+      <!-- 站点检测 -->
+      <dv-border-box-8>
+        <div class="item">
+          <div class="title">重点站点</div>
+          <dv-scroll-board
+            :config="config"
+            style="width: 50rem; height: 23rem"
+          />
         </div>
+      </dv-border-box-8>
+      <div class="item">
+        <!-- 中心 -->
+        <dv-border-box-1>
+          <dv-flyline-chart-enhanced
+            :config="configEnhanced"
+            style="width: 100%; height: 100%"
+          />
+        </dv-border-box-1>
       </div>
-      <div class="item"></div>
-      <div class="item"></div>
+      <dv-border-box-8>
+        <div class="item">
+          <!-- 数据排名 -->
+          <div class="title">数据排名</div>
+          <dv-scroll-board
+            :config="configDate"
+            style="width: 50rem; height: 23rem"
+          />
+        </div>
+      </dv-border-box-8>
+      <dv-border-box-1>
+        <div class="item">
+          <!-- 24小时趋势图 -->
+          <div id="linechart"></div>
+        </div>
+      </dv-border-box-1>
+      <dv-border-box-8 :reverse="true">
+        <div class="item">
+          <!-- 报警排名 -->
+          <div class="title">数据排名</div>
+          <dv-scroll-board
+            :config="configErrer"
+            style="width: 50rem; height: 23rem"
+          />
+        </div>
+      </dv-border-box-8>
+      <dv-border-box-1>
+        <div class="item flex-ja-center">
+          <!-- 重点污染物 -->
+          <div id="keypollutants"></div>
+          <div class="keypollutants-title">
+            天跑1号结炉 425.39/500(报警mg/m"凯气化数 <br />
+
+            国货定电度气 408.8/11500报警m'/s 度气
+          </div>
+        </div>
+      </dv-border-box-1>
+      <dv-border-box-6 class="item">
+        <div class="equipment">
+          <div>
+            <div id="equipment-online"></div>
+            <div class="c-06bbcf">设备在线率</div>
+          </div>
+          <div>
+            <div id="equipment-warn"></div>
+            <div class="c-06bbcf">设备报警率</div>
+          </div>
+          <div>
+            <div id="equipment-datavalid"></div>
+            <div class="c-06bbcf">数据有效率</div>
+          </div>
+        </div>
+      </dv-border-box-6>
+      <dv-border-box-1>
+        <div class="item">
+          <div id="monitor-preview"></div>
+        </div>
+      </dv-border-box-1>
     </div>
   </div>
 </template>
+
 
 <script>
 import Charts from "@jiaminghi/charts";
@@ -147,23 +203,27 @@ export default {
         columnWidth: [100, 200, 100, 100],
         align: ["center", "center", "center", "center"],
       },
-      // 折线图
+      // 折线图24小时趋势图
       linechartOption: {
         title: {
-          text: "周销售额趋势",
+          text: "24小时趋势图",
+          style: {
+            fill: "#06bbcf",
+            fontSize: "20",
+          },
         },
         xAxis: {
-          name: "时段",
-          data: ["0点", "6点", "12点", "18点",],
+          name: "",
+          data: ["0点", "6点", "12点", "18点"],
           boundaryGap: false,
         },
         yAxis: {
-          name: "m²/s",
+          name: "",
           data: "value",
         },
         series: [
           {
-            data: [1200, 2230, 1900, 2100, ],
+            data: [1200, 2230, 1900, 2100],
             type: "line",
           },
         ],
@@ -174,8 +234,8 @@ export default {
           {
             type: "gauge",
             // radius: '90',
-            startAngle: -Math.PI / 2,
-            endAngle: Math.PI * 1.5,
+            startAngle: -Math.PI / 4,
+            endAngle: Math.PI * 2,
             arcLineWidth: 10,
             data: [
               {
@@ -228,12 +288,364 @@ export default {
               show: true,
               formatter: " ",
               position: "start",
-              offset: [-10, 0],
+              offset: [-10, -10],
               style: {
                 fill: "#1ed3e5",
-                fontSize: 13,
-                textAlign: "right",
+                fontSize: 10,
+                // textAlign: "center",
               },
+            },
+          },
+        ],
+      },
+      // 中心大图
+      configEnhanced: {
+        points: [
+          {
+            name: "郑州",
+            coordinate: [0.48, 0.35],
+            halo: {
+              show: true,
+            },
+
+            text: {
+              show: true,
+            },
+          },
+          {
+            name: "新乡",
+            coordinate: [0.52, 0.23],
+          },
+          {
+            name: "焦作",
+            coordinate: [0.43, 0.29],
+          },
+          {
+            name: "开封",
+            coordinate: [0.59, 0.35],
+          },
+          {
+            name: "许昌",
+            coordinate: [0.53, 0.47],
+          },
+          {
+            name: "平顶山",
+            coordinate: [0.45, 0.54],
+          },
+          {
+            name: "洛阳",
+            coordinate: [0.36, 0.38],
+          },
+          {
+            name: "周口",
+            coordinate: [0.62, 0.55],
+            halo: {
+              show: true,
+              color: "#8378ea",
+            },
+          },
+          {
+            name: "漯河",
+            coordinate: [0.56, 0.56],
+          },
+          {
+            name: "南阳",
+            coordinate: [0.37, 0.66],
+            halo: {
+              show: true,
+              color: "#37a2da",
+            },
+          },
+          {
+            name: "信阳",
+            coordinate: [0.55, 0.81],
+          },
+          {
+            name: "驻马店",
+            coordinate: [0.55, 0.67],
+          },
+          {
+            name: "济源",
+            coordinate: [0.37, 0.29],
+          },
+          {
+            name: "三门峡",
+            coordinate: [0.2, 0.36],
+          },
+          {
+            name: "商丘",
+            coordinate: [0.76, 0.41],
+          },
+          {
+            name: "鹤壁",
+            coordinate: [0.59, 0.18],
+          },
+          {
+            name: "濮阳",
+            coordinate: [0.68, 0.17],
+          },
+          {
+            name: "安阳",
+            coordinate: [0.59, 0.1],
+          },
+        ],
+        lines: [
+          {
+            source: "新乡",
+            target: "郑州",
+          },
+          {
+            source: "焦作",
+            target: "郑州",
+          },
+          {
+            source: "开封",
+            target: "郑州",
+          },
+          {
+            source: "周口",
+            target: "郑州",
+            color: "#fb7293",
+            width: 2,
+          },
+          {
+            source: "南阳",
+            target: "郑州",
+            color: "#fb7293",
+            width: 2,
+          },
+          {
+            source: "济源",
+            target: "郑州",
+          },
+          {
+            source: "三门峡",
+            target: "郑州",
+          },
+          {
+            source: "商丘",
+            target: "郑州",
+          },
+          {
+            source: "鹤壁",
+            target: "郑州",
+          },
+          {
+            source: "濮阳",
+            target: "郑州",
+          },
+          {
+            source: "安阳",
+            target: "郑州",
+          },
+          {
+            source: "许昌",
+            target: "南阳",
+            color: "#37a2da",
+          },
+          {
+            source: "平顶山",
+            target: "南阳",
+            color: "#37a2da",
+          },
+          {
+            source: "洛阳",
+            target: "南阳",
+            color: "#37a2da",
+          },
+          {
+            source: "驻马店",
+            target: "周口",
+            color: "#8378ea",
+          },
+          {
+            source: "信阳",
+            target: "周口",
+            color: "#8378ea",
+          },
+          {
+            source: "漯河",
+            target: "周口",
+            color: "#8378ea",
+          },
+        ],
+        // icon: {
+        //   show: true,
+        //   // src: "/img/flylineChart/mapPoint.png",
+        // },
+        text: {
+          show: true,
+        },
+        k: 0.5,
+        bgImgSrc: "@/assets/images/3d.jpg",
+      },
+      // 设备在线率
+      equipmentOnline: {
+        series: [
+          {
+            type: "gauge",
+            startAngle: -Math.PI / 2,
+            endAngle: Math.PI * 1.5,
+            arcLineWidth: 10,
+            data: [
+              {
+                name: "itemA",
+                value: 65,
+                gradient: ["#2fded6", "#5093ff", "#41aaff"],
+              },
+            ],
+            axisLabel: {
+              show: false,
+            },
+            axisTick: {
+              show: false,
+            },
+            pointer: {
+              show: false,
+            },
+            dataItemStyle: {
+              lineCap: "round",
+            },
+            details: {
+              show: true,
+              formatter: "{value}%",
+              style: {
+                fill: "#1ed3e5",
+                fontSize: 35,
+              },
+            },
+          },
+        ],
+      },
+      // 设备报警率
+      equipmentWarn: {
+        series: [
+          {
+            type: "gauge",
+            startAngle: -Math.PI / 2,
+            endAngle: Math.PI * 1.5,
+            arcLineWidth: 10,
+            data: [
+              {
+                name: "itemA",
+                value: 65,
+                gradient: ["#03c2fd", "#1ed3e5", "#2fded6"],
+              },
+            ],
+            axisLabel: {
+              show: false,
+            },
+            axisTick: {
+              show: false,
+            },
+            pointer: {
+              show: false,
+            },
+            dataItemStyle: {
+              lineCap: "round",
+            },
+            details: {
+              show: true,
+              formatter: "{value}%",
+              style: {
+                fill: "#1ed3e5",
+                fontSize: 35,
+              },
+            },
+          },
+        ],
+      },
+      // 数据有效率
+      equipmentDatavalid: {
+        series: [
+          {
+            type: "gauge",
+            startAngle: -Math.PI / 1,
+            endAngle: Math.PI * 1,
+            arcLineWidth: 10,
+            data: [
+              {
+                name: "itemA",
+                value: 80,
+                gradient: ["#50b0ff", "#1ed3e5", "#2fded6"],
+              },
+            ],
+            axisLabel: {
+              show: false,
+            },
+            axisTick: {
+              show: false,
+            },
+            pointer: {
+              show: false,
+            },
+            dataItemStyle: {
+              lineCap: "round",
+            },
+            details: {
+              show: true,
+              formatter: "{value}%",
+              style: {
+                fill: "#1ed3e5",
+                fontSize: 35,
+              },
+            },
+          },
+        ],
+      },
+      // 数据排名
+      configDate: {
+        header: ["站点名称", "检测数值(m²/s)", "限值", "超标率"],
+        data: [
+          ["xx定电废气", "408 80", "11500 000", "--"],
+          ["xx1号焦炉", "2463", "11500 000", "--"],
+          ["xx1号气排口", "21 05", "11500 000", "--"],
+          ["xx2号气排口", "行4列2", "11500 000", "--"],
+          ["xx2号焦炉", "行5列2", "11500 000", "--"],
+          ["xx1号焦炉", "2463", "11500 000", "--"],
+          ["xx1号气排口", "21 05", "11500 000", "--"],
+        ],
+        columnWidth: [100, 200, 100, 100],
+        align: ["center", "center", "center", "center"],
+      },
+      // 报警排名
+      configErrer: {
+        header: ["站点名称", "报警次数", "最近一次报警时间"],
+        data: [
+          ["xx定电废气", "--", "--"],
+          ["xx1号焦炉", "--", "--"],
+          ["xx1号气排口", "--", "--"],
+          ["xx2号气排口", "--", "--"],
+          ["xx2号焦炉", "--", "--"],
+          ["xx1号气排口", "--", "--"],
+          ["xx2号气排口", "--", "--"],
+        ],
+        columnWidth: [100, 200, 200],
+        align: ["center", "center", "center"],
+      },
+      // 监测预览
+      monitorPreview: {
+        title: {
+          text: "监测预览",
+          style: {
+            fill: "#06bbcf",
+            fontSize: "20",
+          },
+        },
+        xAxis: {
+          name: "",
+          data: ["废气", "氮氧化物", "二氧化硫", "烟尘", "沥青烟"],
+        },
+        yAxis: {
+          name: "",
+          data: "value",
+        },
+        series: [
+          {
+            data: [3, 1, 2, 3, 4, 5],
+            type: "bar",
+            gradient: {
+              color: ["#37a2da", "#67e0e3"],
             },
           },
         ],
@@ -243,6 +655,10 @@ export default {
   mounted() {
     this.echarts("linechart", this.linechartOption);
     this.echarts("keypollutants", this.keyPollutants);
+    this.echarts("equipment-online", this.equipmentOnline);
+    this.echarts("equipment-warn", this.equipmentWarn);
+    this.echarts("equipment-datavalid", this.equipmentDatavalid);
+    this.echarts("monitor-preview", this.monitorPreview);
   },
   methods: {
     echarts(e, options) {
@@ -259,12 +675,12 @@ export default {
 <style scoped>
 .pollution {
   background-color: black;
+  box-sizing: border-box;
   height: 100vh;
 }
 
 .header {
   gap: 3rem;
-  padding-top: 3rem;
 }
 
 .header .title {
@@ -283,6 +699,7 @@ export default {
     "left-aside_2 center center right-aside_2"
     "left-aside_3 center-2 center-2 right-aside_3";
   gap: 1rem;
+  box-sizing: border-box;
 }
 
 .item {
@@ -338,14 +755,52 @@ export default {
   color: #7ff2ff;
 }
 
-#linechart,
-#keypollutants {
-  width: 100%;
-  height: 100%;
+.c-06bbcf {
+  color: #06bbcf;
 }
 
-.keypollutants-title{
-    color: #2c88f2;
-    color: #01c0d5;
+#linechart,
+#keypollutants,
+#monitor-preview {
+  width: 100%;
+  height: 25rem;
+}
+#keypollutants {
+  position: relative;
+  right: 2rem;
+}
+
+.keypollutants-title {
+  color: #01c0d5;
+}
+
+#equipment-online,
+#equipment-warn,
+#equipment-datavalid {
+  width: 20rem;
+  height: 20rem;
+}
+
+.equipment {
+  text-align: center;
+  display: flex;
+  justify-content: space-around;
+  /* align-items: center; */
+}
+
+.item .title {
+  color: #06bbcf;
+  font-size: 1.7rem;
+  font-weight: 900;
+  text-align: center;
+}
+
+#linechart {
+  position: relative;
+  left: 1.4rem;
+}
+#monitor-preview {
+  position: relative;
+  left: 1rem;
 }
 </style>
