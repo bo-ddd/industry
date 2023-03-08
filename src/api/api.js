1b516dd93a631fe8c24a86e6d1960c95b5e55f3c
wpp <357281221@qq.com>
2023-03-08 15:24:45 +0800

    模块添加权限号

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL : '/api',
-    timeout:5000,
-    headers:{'content-type' : 'application/json'}
+  baseURL: '/api',
+  timeout: 5000,
+  headers: { 'content-type': 'application/json' }
 })
 
 const getPostConfig = function () {
@@ -16,11 +16,11 @@ const getPostConfig = function () {
 }
 // 添加请求拦截器
 instance.interceptors.request.use(function (config) {
-    // 在发送请求之前做些什么
-    // if(config.headers){
-      config.headers.Authorization = sessionStorage.getItem('token');
-    
-    
+  // 在发送请求之前做些什么
+  // if(config.headers){
+  config.headers.Authorization = sessionStorage.getItem('token');
+
+
   return config;
 }, function (error) {
   return Promise.reject(error);
@@ -38,12 +38,16 @@ instance.interceptors.response.use(function (response) {
   return Promise.reject(error);
 })
 
-  export const loginApi = function (payload = {}) {
-    //payload 是不是服务端要的入参
-    //就是网页的预览
-    return instance.post('/auth/login', payload ,getPostConfig())
-  }
-  //获取用户信息
-  export const getUserListApi = function (payload = {}) {
-    return instance.get('/user', payload)
-  }
+export const loginApi = function (payload = {}) {
+  //payload 是不是服务端要的入参
+  //就是网页的预览
+  return instance.post('/auth/login', payload, getPostConfig())
+}
+//获取用户信息
+export const getUserListApi = function (payload = {}) {
+  return instance.get('/user', payload)
+}
+//用户注册
+export const registerUserApi = function (payload = {}) {
+  return instance.post('/auth/register', payload)
+}
