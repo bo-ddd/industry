7acb62b69253c8fcd78209c0a2e312b976903482
ssj <2226435102@qq.com>
2023-03-09 09:20:17 +0800

    隐式登录

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,33 @@ export default {
       }
     },
     methods:{
+      getHourDate(n){
+                    var myDate = new Date(); //实例一个时间对象；                    
+                    //延长时间
+                    myDate.setMinutes(myDate.getMinutes()+n);
+                    var year = myDate.getFullYear();
+                    var month = myDate.getMonth();
+                    var day = myDate.getDate();
+                    var hour=myDate.getHours(); //获取时，
+                    var min=myDate.getMinutes(); //分                    
+                    var seconds="00"; //秒
+                //    console.log("获取时="+hour+"=分="+min+"=秒="+seconds);
+                    if (hour<10) {
+                        hour= "0"+hour;
+                    }
+                    if(min<10){
+                        min="0"+min;
+                    }
+                    if(month<10){
+                        month="0"+month;
+                    }
+                    if(day<10){
+                        day="0"+day;
+                    }
+                    var mytime=year+"-"+month+"-"+day+" "+hour+":"+min+":"+seconds;  
+                    //console.log("返回的时分秒="+mytime);
+                    return mytime;
+                },
       //刷新页面时判断token是否失效
       ifToken(){
        let time = sessionStorage.getItem('time')
@@ -45,15 +72,16 @@ export default {
   //监听
     watch:{
       // 监听vuex中的token的变化
-      // Token(newName, oldName){
+      Token(newName, oldName){
         //倒计时八分钟获取新token
-      //   setTimeout(() => {
-      //      getToken({}).then(res => {
-      //       this.$store.state.token = res.data.data.access_token
-      //       sessionStorage.setItem("token", res.data.data.access_token);
-      //      })
-      //    }, 480000);
-      //   }
+        setTimeout(() => {
+           getToken({}).then(res => {
+            this.$store.state.token = res.data.data.access_token
+            this.$store.state.time = new Date(this.getHourDate(8)).getTime()
+            sessionStorage.setItem("token", res.data.data.access_token);
+           })
+         }, 480000);
+        }
     }
 }
 
