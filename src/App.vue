ae2c0e16d134fa8fb8a3eda3e22c31484538c3c7
ssj <2226435102@qq.com>
2023-03-09 15:05:17 +0800

    隐式登录

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,106 @@
 <template>
   <div id="app">
-   <router-view></router-view>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import { getToken } from "@/api/api";
 export default {
-    data(){
-      return {
-
+  data() {
+    return {};
+  },
+  methods: {
+    getHourDate(n, a) {
+      var myDate = a == null ? new Date() : a;
+      //延长时间
+      myDate.setMinutes(myDate.getMinutes() + n);
+      var year = myDate.getFullYear();
+      var month = myDate.getMonth() + 1;
+      var day = myDate.getDate();
+      var hour = myDate.getHours(); //获取时，
+      var min = myDate.getMinutes(); //分
+      var seconds = myDate.getMinutes(); //秒
+      if (hour < 10) {
+        hour = "0" + hour;
       }
+      if (min < 10) {
+        min = "0" + min;
+      }
+      if (month < 10) {
+        month = "0" + month;
+      }
+      if (day < 10) {
+        day = "0" + day;
+      }
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+      var mytime =
+        year + "-" + month + "-" + day + " " + hour + ":" + min + ":" + seconds;
+      //console.log("返回的时分秒="+mytime);
+      return mytime;
     },
-    methods:{
-      getHourDate(n){
-                    var myDate = new Date(); //实例一个时间对象；                    
-                    //延长时间
-                    myDate.setMinutes(myDate.getMinutes()+n);
-                    var year = myDate.getFullYear();
-                    var month = myDate.getMonth();
-                    var day = myDate.getDate();
-                    var hour=myDate.getHours(); //获取时，
-                    var min=myDate.getMinutes(); //分                    
-                    var seconds="00"; //秒
-                //    console.log("获取时="+hour+"=分="+min+"=秒="+seconds);
-                    if (hour<10) {
-                        hour= "0"+hour;
-                    }
-                    if(min<10){
-                        min="0"+min;
-                    }
-                    if(month<10){
-                        month="0"+month;
-                    }
-                    if(day<10){
-                        day="0"+day;
-                    }
-                    var mytime=year+"-"+month+"-"+day+" "+hour+":"+min+":"+seconds;  
-                    //console.log("返回的时分秒="+mytime);
-                    return mytime;
-                },
-      //刷新页面时判断token是否失效
-      ifToken(){
-       let time = sessionStorage.getItem('time')
-       let nowTime = new Date().getTime()
-      //  console.log(nowTime - time);
-        if (time < nowTime ) {
+    //刷新页面时判断token是否失效
+    ifToken() {
+      let time = sessionStorage.getItem("time")
+      console.log(Number(time)+488000);
+      
+      if (time == null) {
+        console.log("第一次打开");
+      } else {
+        let nowTime = new Date().getTime()
+        console.log(nowTime);
+        
+console.log(Number(time)+488000);
+        if (Number(time)+488000 < nowTime ) {
+          console.log("刷新页面时token失效");
           this.$router.push({
-                name: "login",
-            });
-        }else{
-
-          getToken({}).then(res => {
-              this.$store.state.token = res.data.data.access_token
-              sessionStorage.setItem("token", res.data.data.access_token);
-              console.log(res.data.data.access_token);
-             })
+            name: "login",
+          });
+        } else {
+          getToken({}).then((res) => {
+            this.$store.state.token = res.data.data.access_token;
+            sessionStorage.setItem("token", res.data.data.access_token);
+            this.$store.state.time = new Date().getTime();
+            console.log(res.data.data.access_token);
+          });
+          console.log("刷新页面时token未失效");
         }
       }
-
+      // console.log(new Date(nowTime).getTime());
     },
-    computed:{
-		Token(){
-			return this.$store.state.token
-		},
-	},
-  created(){
+  },
+  computed: {
+    Token() {
+      return this.$store.state.token;
+    },
+  },
+  created() {
     //进入页面第一时间调用
-     this.ifToken()
+    this.ifToken();
   },
   //监听
-    watch:{
-      // 监听vuex中的token的变化
-      Token(newName, oldName){
-        //倒计时八分钟获取新token
-        setTimeout(() => {
-           getToken({}).then(res => {
-            this.$store.state.token = res.data.data.access_token
-            this.$store.state.time = new Date(this.getHourDate(8)).getTime()
-            sessionStorage.setItem("token", res.data.data.access_token);
-           })
-         }, 480000);
-        }
-    }
-}
+  watch: {
+    // 监听vuex中的token的变化
+    Token(newName, oldName) {
+      //倒计时八分钟获取新token
 
+      setTimeout(() => {
+        console.log("重新获取token");
+        getToken({}).then((res) => {
+          this.$store.state.token = res.data.data.access_token;
+          this.$store.state.time = new Date(this.getHourDate(0)).getTime();
+          sessionStorage.setItem("token", res.data.data.access_token);
+        });
+      }, 480000);
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-body{
+body {
   margin: 0;
   padding: 0;
 }
